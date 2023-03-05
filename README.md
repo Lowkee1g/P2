639344d390a34aa1a0d8f97cde9d1c2e0626a02b
davidfraenkel <david.fraenkel@hotmail.com>
2023-03-05 11:43:05 +0100

    Program structure start up and installed eslint

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # P2
 Yloponom
+
+## Installation
+
+To install dependencies write:
+
+```bash
+npm install
+```
+
+## Usage
+
+To start the server
+
+```bash
+npm run start
+```
+
+You can find the website at localhost:3000
+
+To run eslint write
+
+```bash
+npm run lint
+```
+
--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// …
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const createError = require("http-errors");
+const path = require("path");
+
+const indexRouter = require("./routes/index");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
