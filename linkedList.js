5c2e12f464be1f272b79937b1efc3acf6e396c2c
Anders <andersraae01@gmail.com>
2023-04-27 10:12:24 +0200

    removed linkedlists

--- linkedList.js
+++
@@ -1,8 +0,0 @@
-//Write a function that reverses a linked list in pseudo code
-// 1. Create a new linked list
-// 2. Loop through the original linked list
-// 3. Add each node to the new linked list
-// 4. Return the new linked list
-//
-
-
